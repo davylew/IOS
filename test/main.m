426db33cc6d18c278fca2640f22460cea512ed5e
davylew <davylew@gmail.com>
2013-12-18 02:01:42 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by davylew on 13-12-18.
+//  Copyright (c) 2013年 davylew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
